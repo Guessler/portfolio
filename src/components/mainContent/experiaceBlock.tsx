7123961ef154051e64c14b4752c5c2a01485db3d
ABOBA123 <tarasovlev7@gmail.com>
2024-03-27 20:10:49 +0300

    start-experience

--- src/components/mainContent/experiaceBlock.tsx
+++ src/components/mainContent/experiaceBlock.tsx
@@ -1,15 +1,29 @@
 import {ReactNode} from "react"
+import place from "../../assets/place.svg"
 
 interface experiaceBlockProps {
     children: ReactNode;
-    
+    timeWorking: ReactNode;
+    placeWorking: ReactNode;
 }
 
-function experiaceBlock({children}:experiaceBlockProps){
+function experiaceBlock({ children, timeWorking, placeWorking }:experiaceBlockProps){
     return(
-        <div>
-            {children}
-        </div>
+        <div className="Experience-proto">
+        <div className="flex-row ">
+        {/* <img src={ShariXLogo} alt={ShariXLogo} /> */}
+        <div className="flex-col information-indent small-margin">
+       <p>{children}</p>
+       </div>
+       </div>
+       <div className="flex-col small-margin">
+       <span>{timeWorking}</span>
+       <span className="flex-row">
+       <img style={{marginRight: '2px'}} src={place} alt={place} />
+       <span>{placeWorking}</span>
+       </span>
+       </div>
+    </div>
     )
 }
 export default experiaceBlock;
--- src/components/mainContent/index.tsx
+++ src/components/mainContent/index.tsx
@@ -1,31 +1,15 @@
 import "./style.css"
-import ShariXLogo from "../../assets/big-sharix 1.svg"
-import place from "../../assets/place.svg"
-import experiaceBlock from "./experiaceBlock"
+// import ShariXLogo from "../../assets/big-sharix 1.svg"
+// import place from "../../assets/place.svg"
+import experiaceBlock from './experiaceBlock'
 function MainContent(){
     return(
         <div className="main-content-bg">
             <div className="container">
             <h2>Experience</h2>
-            <div className="Experience-proto">
-                <div className="flex-row ">
-                <img src={ShariXLogo} alt={ShariXLogo} />
-                <div className="flex-col information-indent small-margin">
-               <p>Frontend developer</p>
-               <p>ShariX</p>
-               </div>
-               </div>
-               <experiaceBlock>
-                asdas
-               </experiaceBlock>
-               {/* <div className="flex-col small-margin">
-               <span>Dec 22, still working</span>
-               <span className="flex-row">
-               <img style={{marginRight: '2px'}} src={place} alt={place} />
-               <span>Moscow, Russia</span>
-               </span>
-               </div> */}
-            </div>
+            <experiaceBlock>
+                asijdosjfadsklfjadslfjadskl
+            </experiaceBlock>
             <h2>Latest projects</h2>
             <h2>Education</h2>
             <h2>Skills</h2>
